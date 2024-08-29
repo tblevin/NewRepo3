103017313e9764928e8988eb47deac4aeed02cb6
tblevin <tblevin2@email.tjc.edu>
2024-08-29 10:16:00 -0500

    Add files via upload

---
+++ Multiple_line_.cpp
@@ -0,0 +1,13 @@
+//Talita Blevins
+//This program will display multiples outputs
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "I first surmised the Horses' Heads Were toward Eternity. (Emily Dickinson)" << endl;
+	cout << "Identity theft is not a joke, Jim! Millions of families suffer every year. (The Office U.S)" << endl;
+	cout << "Lorem ipsum dolor sit amet, consectetuer adipiscing elit." << endl;
+}
